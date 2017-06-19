906ab9997ec0532e8f386f96a4f421f73c06aa05
$iwel0 <build@coucounion.io>
2017-06-19 15:13:33 -0500

    SampleDataController.cs : changes to match Summary field with temperature; NavMenu.tsx : proper branding

--- Controllers/SampleDataController.cs
+++ Controllers/SampleDataController.cs
@@ -18,11 +18,14 @@ namespace WebApplicationBasic.Controllers
         public IEnumerable<WeatherForecast> WeatherForecasts(int startDateIndex)
         {
             var rng = new Random();
+            var tmp = Enumerable.Range(1, MAX_DAYS).Select(x => rng.Next(-10, 40)).ToArray();
+            //int bucket = (32 + (int)(tmp / 0.5556)) % 10;
+
             return Enumerable.Range(1, MAX_DAYS).Select(index => new WeatherForecast
             {
                 DateFormatted = DateTime.Now.AddDays(index + startDateIndex).ToString("d"),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
+                TemperatureC = tmp[index - 1], //rng.Next(-20, 55)
+                Summary = Summaries[(32 + (int)(tmp[index - 1] / 0.5556)) % 10]//Summaries[rng.Next(Summaries.Length)]
             });
         }
 
