616a465cca3cf9c852796573461c0ff5677feb5c
$iwel0 <build@coucounion.io>
2017-06-21 19:44:20 -0500

    summaries complete

--- Controllers/SampleDataController.cs
+++ Controllers/SampleDataController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using MoreLinq;
 using System;
 using System.Collections.Generic;
@@ -10,16 +11,22 @@ namespace WebApplicationBasic.Controllers
     public class SampleDataController : Controller
     {
         const int MAX_DAYS = 7;
+        private const int MIN_RANGE_TEMP = -20;
+        private const int MAX_RANGE_TEMP = 55;
         private static string[] Summaries = new[]
         {
-            "Freezing", "Chilly", "Cool", "Mild", "Balmy", "Hot", "Sweltering"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
+
+        private static readonly List<int> tempList = Enumerable.Range(MIN_RANGE_TEMP, MAX_RANGE_TEMP - MIN_RANGE_TEMP).ToList();
+        private static readonly int BATCH_SIZE =
+            Convert.ToInt32(Math.Ceiling((MAX_RANGE_TEMP - MIN_RANGE_TEMP) / (decimal)Summaries.Length));
 
         [HttpGet("[action]")]
         public IEnumerable<WeatherForecast> WeatherForecasts(int startDateIndex)
         {
             var rng = new Random();
-            var tmp = Enumerable.Range(1, MAX_DAYS).Select((x, bucket) => rng.Next(-20, 55)).ToArray();
+            var tmp = Enumerable.Range(1, MAX_DAYS).Select((x, bucket) => rng.Next(MIN_RANGE_TEMP, MAX_RANGE_TEMP)).ToArray();
 
             return tmp.AsEnumerable().Select((x, idx) => new WeatherForecast
             {
@@ -33,29 +40,24 @@ namespace WebApplicationBasic.Controllers
         {
             public string DateFormatted { get; set; }
             public int TemperatureC { get; set; }
-
-            public string Summary
-            {
-
-                get
-                {
-                    int my = 0;
-                    foreach (var band in Enumerable.Range(-20, 55).Batch(10))
-                    {
-                        if (band.Any(i => i == this.TemperatureC))
-                            break;
-                        //return Summaries[my];
-                        my++;
-                    }
-                    return Summaries[my];
-                }
-            }
-
             public int TemperatureF
             {
+                get { return 32 + (int)(TemperatureC / 0.5556); }
+            }
+            public string Summary
+            {
                 get
                 {
-                    return 32 + (int)(TemperatureC / 0.5556);
+                    string summary = null;
+                    tempList.Batch(BATCH_SIZE)
+                        .Select((pr, idx) => new { Index = idx, BatchColl = pr }).AsEnumerable()
+                        .ForEach((band) =>
+                        {
+                            if (band.BatchColl.Any(i => i == TemperatureC))
+                                summary = Summaries[band.Index];
+                        });
+
+                    return summary;
                 }
             }
         }
